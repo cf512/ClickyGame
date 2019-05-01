cd1b70a3cec7fe4883dcbd808d9f28341d12a9ab
Connor F <connorfinnigan@gmail.com>
2019-04-30 20:23:17 -0500

    Updates

---
+++ precache-manifest.e054cb9a0db76b67f0185a9e9434eb55.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4db0501c8f27248da0a5143ca6b049e1",
+    "url": "/clickygame/index.html"
+  },
+  {
+    "revision": "28bb495f327aceb2aa64",
+    "url": "/clickygame/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/clickygame/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "28bb495f327aceb2aa64",
+    "url": "/clickygame/static/js/main.346aff1b.chunk.js"
+  },
+  {
+    "revision": "0f0146524616eea72d1b",
+    "url": "/clickygame/static/js/runtime~main.bb8261cb.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/clickygame/static/media/logo.5d5d9eef.svg"
+  }
+]);
